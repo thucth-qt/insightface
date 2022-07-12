94aed4b41c3a27ac3ff4441823d98db5585f9fcb
ws1 <ws1>
2022-07-12 09:49:35 +0700

    update for training on ws1

--- recognition/arcface_torch/run.sh
+++ recognition/arcface_torch/run.sh
@@ -1,6 +1,6 @@
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch \
---nproc_per_node=8 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
+--nproc_per_node=4 \
 --nnodes=1 \
 --node_rank=0 \
 --master_addr="127.0.0.1" \
