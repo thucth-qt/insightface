14cc492f31c3a79834f88eb5fb9215c1d096c55e
ws1 <ws1>
2022-07-15 11:35:18 +0700

    add script train partialfc

--- recognition/arcface_torch/scripts/run.sh
+++ recognition/arcface_torch/scripts/run.sh
@@ -1,6 +1,6 @@
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch \
---nproc_per_node=4 \
+CUDA_VISIBLE_DEVICES=0,2,3 python -m torch.distributed.launch \
+--nproc_per_node=3 \
 --nnodes=1 \
 --node_rank=0 \
 --master_addr="127.0.0.1" \
